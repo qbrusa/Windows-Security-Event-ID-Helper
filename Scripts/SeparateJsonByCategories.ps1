0c500cc2a99e1285dcda59e043ccb80bdd4ecaa5
Quentin BRUSA <122641985+qbrusa@users.noreply.github.com>
2023-02-21 16:35:33 -0500

    Update SeparateJsonByCategories.ps1

--- Scripts/SeparateJsonByCategories.ps1
+++ Scripts/SeparateJsonByCategories.ps1
@@ -1,7 +1,7 @@
 
 $json = Get-Content .\AdvancedSecurityEventIDs.json | ConvertFrom-Json
 
-$categories = "Account Logon", "Account Management", "Detailed Tracking", "DS Access", "Global Object Access Auditing", "Logon Logoff", "Object Access", "Other Events", "Policy Change", "Privilege Use", "System"
+$categories = "Account Logon", "Account Management", "Detailed Tracking", "DS Access", "Logon Logoff", "Object Access", "Other Events", "Policy Change", "Privilege Use", "System"
 
 foreach ($category in $categories){
 
